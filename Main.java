8d3091dcb471e195eb80dc5215381c0a7907d306
pawara101 <pawara101dassanayake@gmail.com>
2023-02-11 11:47:18 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    //The main() method is required.Any code inside the main() method will be executed.hat every program must contain the main() method.
+    public static void main(String[] args) { 
+      System.out.println("Hello World");
+      System.out.print("Pawara!!!!"); // Only valid Double quotes
+      System.out.println(200*3);
+
+      
+    }
+  }
