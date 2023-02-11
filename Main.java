86e628241e40057d5e3fec8afddc437d9b12cd2a
pawara101 <pawara101dassanayake@gmail.com>
2023-02-11 11:52:34 +0530

    second commit

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) { 
       System.out.println("Hello World");
       System.out.print("Pawara!!!!"); // Only valid Double quotes
-      System.out.println(200*3);
+      System.out.println(200*3); 
 
-      
+      // Variables
+      String name = "John";
+      System.out.println(name);
+
     }
   }
